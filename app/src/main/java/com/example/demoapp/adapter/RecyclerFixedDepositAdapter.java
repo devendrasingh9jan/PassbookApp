42fac069332ed9516f247a07f4d93ff8c1e35b74
devendra.singh <devendra.singh@techlensglobal.com>
2023-11-30 04:56:01 +0530

    completed fd layout view

--- app/src/main/java/com/example/demoapp/adapter/RecyclerFixedDepositAdapter.java
+++ app/src/main/java/com/example/demoapp/adapter/RecyclerFixedDepositAdapter.java
@@ -5,6 +5,7 @@ import android.content.res.ColorStateList;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -14,12 +15,16 @@ import com.example.demoapp.R;
 import com.example.demoapp.model.FdViewHolder;
 import com.example.demoapp.model.FixedDeposit;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class RecyclerFixedDepositAdapter extends RecyclerView.Adapter<FdViewHolder> {
 
     private Context context;
     private List<FixedDeposit> fixedDepositList;
+
+    private int expandedPosition = -1; // Initially, no item is expanded
+
 
     public RecyclerFixedDepositAdapter(Context context, List<FixedDeposit> fixedDepositList) {
         this.context = context;
@@ -36,20 +41,59 @@ public class RecyclerFixedDepositAdapter extends RecyclerView.Adapter<FdViewHold
     @Override
     public void onBindViewHolder(@NonNull FdViewHolder holder, int position) {
         FixedDeposit fixedDeposit = fixedDepositList.get(position);
-        holder.amountValueView.setText(String.valueOf(fixedDeposit.getAmount()));
-        holder.tenureValueView.setText(String.valueOf(fixedDeposit.getTenure()));
+        holder.numberView.setText(String.valueOf(fixedDeposit.getNumber()));
+        holder.maturityAmountView.setText("\u20B9"+String.valueOf(fixedDeposit.getMaturityAmount()));
+        holder.amountValueView.setText("\u20B9"+String.valueOf(fixedDeposit.getAmount()));
+        holder.tenureValueView.setText(String.valueOf(fixedDeposit.getTenure())+" "+"days");
         holder.daysLeftView.setText(String.valueOf(fixedDeposit.getDaysLeft()));
+        holder.rateView.setText(fixedDeposit.getRate() + "%");
         int daysLeft = fixedDeposit.getDaysLeft();
         int totalTenure = fixedDeposit.getTenure();
         int maxProgressBarValue = 100;
-
         // Calculate progress based on the remaining days
         int normalizedProgress = (int) ((double) daysLeft / totalTenure * maxProgressBarValue);
-
         // Set the progress and color based on the normalized progress
         holder.progressBarDaysLeft.setProgress(normalizedProgress);
         holder.progressBarDaysLeft.setProgressTintList(ColorStateList.valueOf(getColorForProgress(normalizedProgress)));
+
+        // Set visibility based on the expanded position
+        holder.expandableLayout.setVisibility(position == expandedPosition ? View.VISIBLE : View.GONE);
+
+        holder.fdCardView.setOnClickListener(view -> {
+            if (position != expandedPosition) {
+                // Collapse the currently expanded item
+                notifyItemChanged(expandedPosition);
+                expandedPosition = holder.getAdapterPosition();
+            }
+
+            if (holder.expandableLayout.getVisibility() == View.VISIBLE) {
+                holder.expandableLayout.animate()
+                        .alpha(0f)
+                        .setDuration(500) // Animation duration
+                        .setInterpolator(new AccelerateDecelerateInterpolator())
+                        .withEndAction(() -> holder.expandableLayout.setVisibility(View.GONE))
+                        .start();
+            } else {
+                holder.expandableLayout.setAlpha(0f);
+                holder.expandableLayout.setVisibility(View.VISIBLE);
+                holder.expandableLayout.animate()
+                        .alpha(1f)
+                        .setDuration(500) // Animation duration
+                        .setInterpolator(new AccelerateDecelerateInterpolator())
+                        .start();
+
+                LocalDate createdDate = fixedDeposit.getCreatedDate();
+                LocalDate endDate = fixedDeposit.getEndDate();
+                holder.startDateView.setText(createdDate.getYear()+"-"+String.format("%02d", createdDate.getMonthValue())+"-"+
+                        String.format("%02d", createdDate.getDayOfMonth()));
+                holder.endDateView.setText(endDate.getYear()+"-"+String.format("%02d", endDate.getMonthValue())+"-"+
+                        String.format("%02d", endDate.getDayOfMonth()));
+                holder.notesValueView.setText(fixedDeposit.getNotes());
+                holder.bankAddressView.setText(fixedDeposit.getBankWithAddress());
+            }
+        });
     }
+
 
     private int getColorForProgress(int progress) {
         if (progress >= 75) {
--- app/src/main/java/com/example/demoapp/model/FdViewHolder.java
+++ app/src/main/java/com/example/demoapp/model/FdViewHolder.java
@@ -2,17 +2,24 @@ package com.example.demoapp.model;
 
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.demoapp.R;
 
 public class FdViewHolder extends RecyclerView.ViewHolder {
 
-    public TextView amountValueView, tenureValueView, daysLeftView;
+    public TextView amountValueView, tenureValueView, daysLeftView, startDateView, endDateView,
+            notesValueView, maturityAmountView, bankAddressView, rateView, numberView;
     public ProgressBar progressBarDaysLeft;
+    public CardView fdCardView;
+    public RelativeLayout expandableLayout;
+
+
     public FdViewHolder(@NonNull View itemView) {
         super(itemView);
         initializeFields(itemView);
@@ -23,5 +30,14 @@ public class FdViewHolder extends RecyclerView.ViewHolder {
         tenureValueView = itemView.findViewById(R.id.textViewTenureValue);
         daysLeftView = itemView.findViewById(R.id.textViewDaysLeftValue);
         progressBarDaysLeft = itemView.findViewById(R.id.progressBarDaysLeft);
+        fdCardView = itemView.findViewById(R.id.fdCardView);
+        expandableLayout = itemView.findViewById(R.id.expandableLayout);
+        notesValueView = itemView.findViewById(R.id.textViewNotesValue);
+        startDateView = itemView.findViewById(R.id.textViewStartDateValue);
+        endDateView = itemView.findViewById(R.id.textViewEndDateValue);
+        maturityAmountView = itemView.findViewById(R.id.textViewMaturityAmountValue);
+        bankAddressView = itemView.findViewById(R.id.textViewBankAddressValue);
+        rateView = itemView.findViewById(R.id.textViewRateValue);
+        numberView = itemView.findViewById(R.id.textViewNumber);
     }
 }
